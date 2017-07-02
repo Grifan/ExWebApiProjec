5f57dfb9fa3fec1ff006a04ca273edd5fc1dac4e
Grifan <grifaaan@gmail.com>
2017-07-03 00:57:25 +0300

    DB's migrations were executed (th metanit). Add auth and reg links at Home page

---
+++ ExWebApiProjec/Migrations/201707022026106_MigrateDB.cs
@@ -0,0 +1,53 @@
+namespace ExWebApiProjec.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MigrateDB : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Roles",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Tasks",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Description = c.String(),
+                        CreationDate = c.DateTime(nullable: false),
+                        Start = c.DateTime(nullable: false),
+                        Finish = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Email = c.String(),
+                        Password = c.String(),
+                        CreationDate = c.DateTime(nullable: false),
+                        RoleId = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+            DropTable("dbo.Tasks");
+            DropTable("dbo.Roles");
+        }
+    }
+}
